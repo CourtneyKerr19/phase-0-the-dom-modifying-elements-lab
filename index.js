d6e2bf6b9c8d31247b150d12590f15ee26ad1019
Courtney Kerr <courtneykerr219@gmail.com>
2024-01-02 07:30:08 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Write your code here! 
+
+// Remove the 'main#main' DOM node
+const main = document.querySelector('main#main');
+main.remove();
+
+// Create a new <h1> element with id 'victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Set the text content of the new <h1> element
+newHeader.textContent = "Courtney is the champion";
+
+// Append the new <h1> element to the document body
+document.body.appendChild(newHeader);
